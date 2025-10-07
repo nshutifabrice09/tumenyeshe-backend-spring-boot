b6251e10cbd21e0ef4ccecae54b401db27b3ee2b
nshuti.fabrice <nshuti.fabrice09@gmail.com>
2025-10-07 11:15:14 +0200

    generating getters and setters

--- src/main/java/rw/tumenyeshe/model/OutageEvent.java
+++ src/main/java/rw/tumenyeshe/model/OutageEvent.java
@@ -24,5 +24,54 @@ public class OutageEvent {
     private LocalDateTime endTime;
 
     @ManyToOne
+    @JoinColumn(name = "affectedZone_id")
     private Zone affectedZone;
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public LocalDateTime getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(LocalDateTime startTime) {
+        this.startTime = startTime;
+    }
+
+    public LocalDateTime getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(LocalDateTime endTime) {
+        this.endTime = endTime;
+    }
+
+    public Zone getAffectedZone() {
+        return affectedZone;
+    }
+
+    public void setAffectedZone(Zone affectedZone) {
+        this.affectedZone = affectedZone;
+    }
 }
--- src/main/java/rw/tumenyeshe/model/ReportAnalytics.java
+++ src/main/java/rw/tumenyeshe/model/ReportAnalytics.java
@@ -24,4 +24,52 @@ public class ReportAnalytics {
     private int resolved;
 
     private String month;
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public int getTotalReports() {
+        return totalReports;
+    }
+
+    public void setTotalReports(int totalReports) {
+        this.totalReports = totalReports;
+    }
+
+    public int getPending() {
+        return pending;
+    }
+
+    public void setPending(int pending) {
+        this.pending = pending;
+    }
+
+    public int getResolved() {
+        return resolved;
+    }
+
+    public void setResolved(int resolved) {
+        this.resolved = resolved;
+    }
+
+    public String getMonth() {
+        return month;
+    }
+
+    public void setMonth(String month) {
+        this.month = month;
+    }
 }
