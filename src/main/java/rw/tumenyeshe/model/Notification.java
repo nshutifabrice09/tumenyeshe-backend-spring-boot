3ca602a5b4e9cac318b50ec537951621a72fc396
nshuti.fabrice <nshuti.fabrice09@gmail.com>
2025-10-07 11:14:36 +0200

    generating getters and setters

--- src/main/java/rw/tumenyeshe/model/Notification.java
+++ src/main/java/rw/tumenyeshe/model/Notification.java
@@ -20,9 +20,50 @@ public class Notification {
     private UUID id;
 
     @ManyToOne
+    @JoinColumn(name = "recipient_id")
     private User recipient;
 
     private String message;
     private boolean isRead;
     private LocalDateTime sentAt;
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public User getRecipient() {
+        return recipient;
+    }
+
+    public void setRecipient(User recipient) {
+        this.recipient = recipient;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public boolean isRead() {
+        return isRead;
+    }
+
+    public void setRead(boolean read) {
+        isRead = read;
+    }
+
+    public LocalDateTime getSentAt() {
+        return sentAt;
+    }
+
+    public void setSentAt(LocalDateTime sentAt) {
+        this.sentAt = sentAt;
+    }
 }
